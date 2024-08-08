2ff11a32b4aa6eec93b93f33cc9083037266e94c
Mazuwa-akatsuki <mazuwapmsiska@gmail.com>
2024-08-08 14:20:09 +0200

    first commit

---
+++ new work.py
@@ -0,0 +1,60 @@
+#red_bucket = input("what do you want to put in the bucket?  ")
+#print(red_bucket)
+
+#print(5==4)
+
+print("")
+
+#thomas_Age = 10
+#Age_at_kindergarten = 5
+
+#f thomas_Age < Age_at_kindergarten:
+ #   print("Thomas should be in pre-school")
+#elif thomas_Age == Age_at_kindergarten:
+ #   print("enjoy kindergarten")
+#else:
+    #print("thomas should be in another class")
+    
+
+#def print_kevin(text):
+    #print(text)   
+    #print(text)
+     
+#print_kevin("kevin stratvert has a great channel") 
+
+#def school_age_calculator(age,name):
+ #   if age < 5:
+  #      print("Enjoy the time!", name, "is only", age)
+   # elif age == 5: 
+    #    print("enjoy kindergarten", name) 
+    #else:
+     #   print("they grow up so fast!")
+        
+#school_age_calculator(10,"thomas")  
+          
+
+#def add_ten_to_age(age):
+    #new_age = age + 10
+    #return new_age
+
+#how_old_will_i_be = add_ten_to_age(15)
+#print(how_old_will_i_be)
+
+
+# while loop
+x = 0
+#while (x<5): 
+    #print(x)
+    #x = x + 1
+    
+    # for loop
+   # for x in range(5,10):
+        #print(x)
+        
+#days = ["mon","tue","wed","thu","fri","sat","sun"]
+        
+#for d in days:  
+    #if(d=="thu"):break
+   # print(d)
+   
+   
